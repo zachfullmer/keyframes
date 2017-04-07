3590a24ec3d1a49c2b06371427eed0830d3ed366
zachfullmer <zacharyfullmer@mail.weber.edu>
2017-04-07 15:07:17 -0600

    hexToRgb helper added

--- src/Helpers.js
+++ src/Helpers.js
@@ -80,5 +80,14 @@ export function colorNameToHex(color) {
     return false;
 }
 
+export function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? [
+        parseInt(result[1], 16),
+        parseInt(result[2], 16),
+        parseInt(result[3], 16)
+    ] : null;
+}
+
 
 export const degrees = 57.2958;
