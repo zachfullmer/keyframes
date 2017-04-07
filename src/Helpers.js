37f6a7ac6bcf2302e197605f3c02fbee983e60c8
zachfullmer <zacharyfullmer@mail.weber.edu>
2017-04-07 17:33:05 -0600

    rgbToHex helper added

--- src/Helpers.js
+++ src/Helpers.js
@@ -80,6 +80,15 @@ export function colorNameToHex(color) {
     return false;
 }
 
+function componentToHex(c) {
+    var hex = c.toString(16);
+    return hex.length == 1 ? "0" + hex : hex;
+}
+
+export function rgbToHex(r, g, b) {
+    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
+
 export function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
     return result ? [
